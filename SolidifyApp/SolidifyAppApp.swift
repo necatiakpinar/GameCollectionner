f5642cea57ab7562ce30623cf58b66b7fef9c2f0
Necati Akpinar <necatiakpinar1997@gmail.com>
2023-10-21 09:44:57 +0300

    Initial Commit

---
+++ SolidifyApp/SolidifyAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SolidifyAppApp.swift
+//  SolidifyApp
+//
+//  Created by Necati Akpınar on 21.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct SolidifyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
