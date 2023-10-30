2824b74276eeba2b0fd90266dd674fd307b1cb1d
Necati Akpinar <necatiakpinar1997@gmail.com>
2023-10-30 18:48:17 +0300

    game detail view has been refactored .

---
+++ SolidifyApp/Views/GameDetailCommentsView.swift
@@ -0,0 +1,8 @@
+//
+//  GameDetailCommentsView.swift
+//  SolidifyApp
+//
+//  Created by Necati Akpınar on 30.10.2023.
+//
+
+import Foundation
---
+++ SolidifyApp/Views/GameDetailDefinitionView.swift
@@ -0,0 +1,8 @@
+//
+//  GameDetailDefinitionView.swift
+//  SolidifyApp
+//
+//  Created by Necati Akpınar on 30.10.2023.
+//
+
+import Foundation
