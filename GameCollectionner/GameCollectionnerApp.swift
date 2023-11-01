4b661abe47e58189eb9d6b023f65e80da47db194
Necati Akpinar <necatiakpinar1997@gmail.com>
2023-11-01 09:57:38 +0300

    name changed

--- GameCollectionner/GameCollectionnerApp.swift
+++ GameCollectionner/GameCollectionnerApp.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 @main
-struct SolidifyAppApp: App {
+struct GameCollectionnerApp: App {
     
     @ObservedObject var gameEvents = GameEvents()
     var userGames: UserGamesViewModel {
